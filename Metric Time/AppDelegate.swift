0f88e59b06cea3dfff53b095a384ab269136a5d5
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2016-09-22 20:11:40 -0700

    added splash screen on startup (no version number as planned tho)

--- Metric Time/AppDelegate.swift
+++ Metric Time/AppDelegate.swift
@@ -12,11 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+       // sleep(1) //wait a second...
         return true
     }
 
