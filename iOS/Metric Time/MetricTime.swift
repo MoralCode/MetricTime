6de0881efcb16df92a9e0ed8176ab17eeb683fae
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-01-03 20:29:30 -0800

    rearrange/refactor clock-drawing functions

--- iOS/Metric Time/MetricTime.swift
+++ iOS/Metric Time/MetricTime.swift
@@ -17,13 +17,13 @@ import UIKit
 
 class MetricTime {
     
-    let hourLayer = CAShapeLayer()
-    let minuteLayer = CAShapeLayer()
-    let secondLayer = CAShapeLayer()
-    let centerPiece = CAShapeLayer()
     
     var lastCall:Date?
     let clockShouldTick = false
+    //ANALOG CLOCK
+    let clockContext:CGContext? = UIGraphicsGetCurrentContext()
+    var clockView = Clock(frame: CGRect(x: 0, y: 0, width: 230, height: 230))
+
     
     //  the actual time that normal humans use (in millitary time) (actualTime[0] = hour, actualTime[1] = minute, actualTime[2] = second)
     var actualTime: [Int] = [0, 0, 0];
@@ -34,8 +34,6 @@ class MetricTime {
     var seconds = 0.0
     var convertedSeconds = 0.0
 
-    var clockView = UIView(frame: CGRect(x: 0, y: 0, width: 230, height: 230))
-    let clockContext:CGContext? = UIGraphicsGetCurrentContext()
     
     /*
         STYLING
@@ -50,110 +48,231 @@ class MetricTime {
     
     func getContext() -> CGContext { return self.clockContext!}
     
-    func drawAnalogClock() -> UIView {
-        
-        print(clockContext)
-        
-        drawClockFace()
-        //drawClockHands()
-        
-        return clockView
-        
-    }
+    /*
+    THESE FUNCTIONS ARE CALLED WHENEVER A NEW Clock() VIEW IS CREATED
+     */
     
-    func drawClockFace() /*-> UIView */{
+    /// Draws and displays the Arc that makes up the curcumference of the clock face
+    func drawClockCircle() {
         
+        //draw the circle
         clockContext?.addArc(center: CGPoint(x: clockView.bounds.midX, y: clockView.bounds.midY), radius: clockView.bounds.width/2, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        
         clockContext?.drawPath(using: CGPathDrawingMode.fillStroke)
+        
         if clockContext != nil {
             clockContext!.setStrokeColor(UIColor.white.cgColor)
             clockContext!.setLineWidth(4.0)
-            
         }
     }
-    
-    func drawClockHands() -> UIView {
+    /// Calculates the x and y values for the specified number of points around the circle
+    ///
+    /// - parameter forNumbers: Boolean value that is used when calculating the points at which to render the numbers
+    /// - returns: An array of CGPoint's
+    private func getPointsOnCircle(forNumbers:Bool = false) -> [CGPoint] {
         
-       // clockView = UIView(frame: CGRect(x: 0, y: 0, width: 230, height: 230))
+        var desiredNumberOfPoints = 100
+        var inset:CGFloat = 0
+        var adjustment:CGFloat = 0 //this allows you to change the (rotation) position of the output points to adjust the text rendering
+
+    
+        if forNumbers {
+            desiredNumberOfPoints = 10
+            inset = (clockView.bounds.width/2)/numberInset
+            adjustment = -90 //same as 270
+        }
+        
+        var counter = desiredNumberOfPoints //this must stay as is. dont remove the counter variable
+        let tickSpacing = degree2radian(360/CGFloat(desiredNumberOfPoints))
+        let desiredPointRadius = (clockView.bounds.width/2)-inset //the difference between this and the actual radius of the clock is the inset value
+        
+        var points = [CGPoint]()
+        
+        while points.count <= desiredNumberOfPoints {
+            //complex maths... modify at own risk...
+            let x = clockView.bounds.midX - desiredPointRadius * cos(tickSpacing * CGFloat(counter)+degree2radian(adjustment))
+            let y = clockView.bounds.midY - desiredPointRadius * sin(tickSpacing * CGFloat(counter)+degree2radian(adjustment))
+            points.append(CGPoint(x: x, y: y))
+            counter -= 1;
+            print(counter)
+        }
+        print("cut")
+        return points
+        
+    }
+
+    func addTickMarks() {
+        let points = getPointsOnCircle()
+        let tickPath = CGMutablePath()
+        var tickLength: CGFloat
+        
+        for point in points.enumerated() {
+            
+            if point.offset % 10 == 0 {//if the "index" of the point is divisible by 10 w/o remainder
+                tickLength = 2/16
+            } else if point.offset % 5 == 0 {//if the "index" of the point is divisible by 5 w/o remainder
+                tickLength = 3/16
+            } else {
+                tickLength = 1/16
+            }
+            
+            let tickEndPoint = (x: point.element.x + tickLength*(clockView.bounds.midX - point.element.x), y: point.element.y + tickLength*(clockView.bounds.midY - point.element.y))
+            
+            clockContext?.move(to: CGPoint(x: point.element.x, y: point.element.y))
+            clockContext?.addLine(to: CGPoint(x: tickEndPoint.x, y: tickEndPoint.y))
+            clockContext?.addPath(tickPath)
+            
+        }
+        
+        clockContext?.setStrokeColor(clockColor)
+        clockContext?.setLineWidth(3.0)
+        clockContext?.strokePath()
+        
+    }
+    
+    //INCOMPLETE!
+    func addNumbers() {
+        
+        
+        let numberPositions = getPointsOnCircle(forNumbers: true) //inset determines spacing between numbers and edge of the clock
+        
+        // Flip text co-ordinate space, see: http://blog.spacemanlabs.com/2011/08/quick-tip-drawing-core-text-right-side-up/
+        clockContext?.translateBy(x: 0.0, y: clockView.bounds.height)
+        clockContext?.scaleBy(x: 1.0, y: -1.0)
+        
+        let textAttributes = [NSFontAttributeName:numbersFont!, NSForegroundColorAttributeName:clockColor] as CFDictionary
+        
+        // multiplier enables correcting numbering when fewer than 12 numbers are featured, e.g. 4 sides will display 12, 3, 6, 9 (formerly preceeded by a 270 degree adjustment... qué?)
+        let multiplier = 12/10
+        
+        
+        for point in numberPositions.enumerated() {
+            
+            // create the attributed string
+             let str = String(point.offset * multiplier)
+             let text = CFAttributedStringCreate(nil, str as CFString!, textAttributes)
+            
+            
+             // create the line of text
+             let line = CTLineCreateWithAttributedString(text!)
+            
+            
+             // retrieve the bounds of the text
+             let bounds = CTLineGetBoundsWithOptions(line, CTLineBoundsOptions.useOpticalBounds)
+            
+             // set the line width to stroke the text with
+             clockContext?.setLineWidth(1.5)
+            
+             // set the drawing mode to stroke
+             clockContext?.setTextDrawingMode(CGTextDrawingMode.stroke)
+            
+             // Set text position and draw the line into the graphics context, text length and height is adjusted for
+             let xn = point.element.x - bounds.width/2
+             let yn = point.element.y - bounds.midY
+            
+             clockContext?.textPosition = CGPoint(x: xn, y: yn)
+            
+             // the line of text is drawn - see https://developer.apple.com/library/ios/DOCUMENTATION/StringsTextFonts/Conceptual/CoreText_Programming/LayoutOperations/LayoutOperations.html
+             // draw the line of text
+             CTLineDraw(line, clockContext!)
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
+    
+    func addClockHands() {
+        
+        
+        
         
         let hourPath = CGMutablePath()
         let minutePath = CGMutablePath()
         let secondPath = CGMutablePath()
         
-        
-        
-        //set the views to the same size (is this needed? ?(yes)) [views are a little big tho...]
-         hourLayer.frame = clockView.frame
-         minuteLayer.frame = clockView.frame
-         secondLayer.frame = clockView.frame
-         
-        
-         
-         //all hand shapes start in center
-         hourPath.move(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY))
-         minutePath.move(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY))
-         secondPath.move(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY))
+        //all hand shapes start in center
+        hourPath.move(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY))
+        minutePath.move(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY))
+        secondPath.move(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY))
         
         //draw line straight up at specified lengths
-         hourPath.addLine(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY-50))
-         minutePath.addLine(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY-62.5))
-         secondPath.addLine(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY-75))
-         
-         hourLayer.path = hourPath
-         minuteLayer.path = minutePath
-         secondLayer.path = secondPath
+        hourPath.addLine(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY-50))
+        minutePath.addLine(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY-62.5))
+        secondPath.addLine(to: CGPoint(x: clockView.frame.midX, y: clockView.frame.midY-75))
+        
+        //layers are then rotated from these vertical positions
+        
+        
+        let hourLayer = CAShapeLayer()
+        let minuteLayer = CAShapeLayer()
+        let secondLayer = CAShapeLayer()
+        let centerPiece = CAShapeLayer()
+        
+        //set the views to the same size [views are a little big tho...]?
+        hourLayer.frame = clockView.frame
+        minuteLayer.frame = clockView.frame
+        secondLayer.frame = clockView.frame
+        
+        hourLayer.path = hourPath
+        minuteLayer.path = minutePath
+        secondLayer.path = secondPath
         
         //set line thicknesses
-         hourLayer.lineWidth = 4
-         minuteLayer.lineWidth = 3
-         secondLayer.lineWidth = 1
+        hourLayer.lineWidth = 4
+        minuteLayer.lineWidth = 3
+        secondLayer.lineWidth = 1
         
         //set line ending
-         //TODO: merge common variables here for easier customisation
-         hourLayer.lineCap = kCALineCapRound
-         minuteLayer.lineCap = kCALineCapRound
-         secondLayer.lineCap = kCALineCapRound
+        hourLayer.lineCap = kCALineCapRound
+        minuteLayer.lineCap = kCALineCapRound
+        secondLayer.lineCap = kCALineCapRound
+    
         
         //set color
-         hourLayer.strokeColor = UIColor.white.cgColor
-         minuteLayer.strokeColor = UIColor.white.cgColor
-         secondLayer.strokeColor = UIColor.red.cgColor
-         
-         
-         hourLayer.rasterizationScale = UIScreen.main.scale;
-         minuteLayer.rasterizationScale = UIScreen.main.scale;
-         secondLayer.rasterizationScale = UIScreen.main.scale;
-         
-         hourLayer.shouldRasterize = true
-         minuteLayer.shouldRasterize = true
-         secondLayer.shouldRasterize = true
-         
-         
-         let endAngle = CGFloat(2*M_PI)
-         
-         let circle = UIBezierPath(arcCenter: CGPoint(x:clockView.frame.midX,y:clockView.frame.midX), radius: 2.75, startAngle: 0, endAngle: endAngle, clockwise: true)
-         centerPiece.path = circle.cgPath
-         centerPiece.fillColor = UIColor.gray.cgColor
+        hourLayer.strokeColor = UIColor.white.cgColor
+        minuteLayer.strokeColor = UIColor.white.cgColor
+        secondLayer.strokeColor = UIColor.red.cgColor
         
-         
-         
-         clockView.layer.addSublayer(hourLayer)
-         clockView.layer.addSublayer(minuteLayer)
-         clockView.layer.addSublayer(secondLayer)
-         clockView.layer.addSublayer(centerPiece)
-         
-
- 
- 
-        return clockView
+        
+        hourLayer.rasterizationScale = UIScreen.main.scale;
+        minuteLayer.rasterizationScale = UIScreen.main.scale;
+        secondLayer.rasterizationScale = UIScreen.main.scale;
+        
+        hourLayer.shouldRasterize = true
+        minuteLayer.shouldRasterize = true
+        secondLayer.shouldRasterize = true
+        
+    
+        //finally add the center piece
+        
+       // let circle = UIBezierPath(arcCenter: CGPoint(x:clockView.frame.midX,y:clockView.frame.midX), radius: 2.75, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        centerPiece.path = UIBezierPath(arcCenter: CGPoint(x:clockView.frame.midX,y:clockView.frame.midX), radius: 2.75, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true).cgPath
+        centerPiece.fillColor = UIColor.gray.cgColor
+        
+        
+        //add all the layers to clockView as subviews
+        clockView.layer.addSublayer(hourLayer)
+        clockView.layer.addSublayer(minuteLayer)
+        clockView.layer.addSublayer(secondLayer)
+        clockView.layer.addSublayer(centerPiece)
+        
         
     }
     
+
+    
+    
+    //INCOMPLETE!
+
     func updateHandsPosition(metricTime: (hour: Int, minute: Int, second: Int)) {
         
         setHandsAngle(handDegrees: getHandsPosition(metricTime: (hour: metricTime.hour, minute: metricTime.minute, second: metricTime.second)))
     }
-    
+    //WHAT?
     func getHandsPosition(metricTime: (hour: Int, minute: Int, second: Int)) -> (hAngle:CGFloat, mAngle:CGFloat, sAngle:CGFloat) {
         
         var secondsAngle = (Double(metricTime.second)/100)
@@ -254,99 +373,7 @@ class MetricTime {
     
     
     
-    private func getTickMarkLocations(clockView:UIView, forNumbers:Bool = false) -> [CGPoint] {
-        var clockPoints = 100 /* total number of tick marks on the clock */
-        var counter = clockPoints
-        let tickSpacing = degree2radian(360/CGFloat(clockPoints))
-        let clockCenter = (x: clockView.bounds.midX, y: clockView.bounds.midY)
-        let clockRadius = clockView.bounds.width/2
-        let adjustment:CGFloat = 0 //idk what this does, but i dont wanna remove it ciz' i may break the math...
-        var points = [CGPoint]()
-        
-        if forNumbers { clockPoints = 10; counter = clockPoints}
-        
-        while points.count <= clockPoints {
-            //complex maths... dont touch...
-            let xpo = clockCenter.x - clockRadius * cos(tickSpacing * CGFloat(counter)+degree2radian(adjustment))
-            let ypo = clockCenter.y - clockRadius * sin(tickSpacing * CGFloat(counter)+degree2radian(adjustment))
-            points.append(CGPoint(x: xpo, y: ypo))
-            counter -= 1;
-        }
-        
-        return points
-        
-    }
-    
-    func addTickMarks(points:[CGPoint]) {
-        let tickPath = CGMutablePath()
-        var tickLength: CGFloat
-        
-        for point in points.enumerated() {
-            
-            if point.offset % 10 == 0 {//if the "index" of the point is divisible by 10 w/o remainder
-                tickLength = 2/16
-            } else if point.offset % 5 == 0 {//if the "index" of the point is divisible by 5 w/o remainder
-                tickLength = 3/16
-            } else {
-                tickLength = 1/16
-            }
-            
-            let tickEndPoint = (x: point.element.x + tickLength*(clockView.bounds.midX - point.element.x), y: point.element.y + tickLength*(clockView.bounds.midY - point.element.y))
-            
-            clockContext?.move(to: CGPoint(x: point.element.x, y: point.element.y))
-            clockContext?.addLine(to: CGPoint(x: tickEndPoint.x, y: tickEndPoint.y))
-            clockContext?.addPath(tickPath)
-            
-        }
-        
-        clockContext?.setStrokeColor(clockColor)
-        clockContext?.setLineWidth(3.0)
-        clockContext?.strokePath()
-        
-    }
-    
-    func addNumbers() {
-        
-        let inset = 5/16 //clockView.bounds.width/2 //determines spacing between numbers and edge of the clock
-        
-        let numberPositions = getTickMarkLocations(clockView: clockView, forNumbers: true)
-        
-        // Flip text co-ordinate space, see: http://blog.spacemanlabs.com/2011/08/quick-tip-drawing-core-text-right-side-up/
-        clockContext?.translateBy(x: 0.0, y: clockView.bounds.height)
-        clockContext?.scaleBy(x: 1.0, y: -1.0)
-        
-        let textAttributes = [NSFontAttributeName:numbersFont!, NSForegroundColorAttributeName:clockColor] as CFDictionary
-        // multiplier enables correcting numbering when fewer than 12 numbers are featured, e.g. 4 sides will display 12, 3, 6, 9 (formerly preceeded by a 270 degree adjustment... qué?
 
-        let multiplier = 12/10
-
-        
-        for point in numberPositions.enumerated() {
-            
-       /*     // create the attributed string
-            let str = String(point.offset * multiplier)
-            let text = CFAttributedStringCreate(nil, str as CFString!, attr)
-            // create the line of text
-            let line = CTLineCreateWithAttributedString(text!)
-            // retrieve the bounds of the text
-            let bounds = CTLineGetBoundsWithOptions(line, CTLineBoundsOptions.useOpticalBounds)
-            // set the line width to stroke the text with
-            clockContext.setLineWidth(1.5)
-            // set the drawing mode to stroke
-            clockContext.setTextDrawingMode(CGTextDrawingMode.stroke)
-            // Set text position and draw the line into the graphics context, text length and height is adjusted for
-            let xn = p.element.x - bounds.width/2
-            let yn = p.element.y - bounds.midY
-            clockContext.textPosition = CGPoint(x: xn, y: yn)
-            // the line of text is drawn - see https://developer.apple.com/library/ios/DOCUMENTATION/StringsTextFonts/Conceptual/CoreText_Programming/LayoutOperations/LayoutOperations.html
-            // draw the line of text
-            CTLineDraw(line, context)
-*/
-            
-        }
-        
-        
-    }
     
     
 
@@ -361,6 +388,20 @@ class Clock: UIView {
         
         //do things as soon as a new Clock object is created?????
         //probably dont need?
+        let metricTime:MetricTime = MetricTime()//may cause some issues?
+        
+        
+     //   let context = metricTime.getContext()
+       // let context = UIGraphicsGetCurrentContext()
+        
+       // context.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: CGFloat(metricTime.clockRadius), startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+       // context.drawPath(using: CGPathDrawingMode.fillStroke);
+        metricTime.drawClockCircle()
+        metricTime.addTickMarks()
+        metricTime.addNumbers()
+        metricTime.addClockHands()
+        
+        
         
     }
     
